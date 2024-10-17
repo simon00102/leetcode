1d0c685cf2e94b2d4d3f5159fd14294eef33c486
Simon Lee <simon00102@gmail.com>
2024-10-17 17:45:07 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "fmt/core.h"
+#include "p2/p2.hpp"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    p2();
+    return 0;
+}
---
+++ src/test2.cpp
@@ -0,0 +1,11 @@
+#include "p1/add.hpp"
+#include "p2/p2.hpp"
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    add(1, 2);
+    p2();
+    return 0;
+}
